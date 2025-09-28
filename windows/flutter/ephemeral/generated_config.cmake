b902cddf3ca605b6857fa8424143192bfe1d9745
icheh360 <Info.icheh@gmail.com>
2025-09-28 16:58:22 +0330

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Programs\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Projects\\Flutter\\REYE\\reye" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Programs\\Flutter\\flutter"
+  "PROJECT_DIR=E:\\Projects\\Flutter\\REYE\\reye"
+  "FLUTTER_ROOT=E:\\Programs\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Projects\\Flutter\\REYE\\reye\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Projects\\Flutter\\REYE\\reye"
+  "FLUTTER_TARGET=E:\\Projects\\Flutter\\REYE\\reye\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzUuNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDY5M2I0YjlkYg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YzI5ODA5MTM1MQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My45LjI="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Projects\\Flutter\\REYE\\reye\\.dart_tool\\package_config.json"
+)
